7b51f6f903e18e47da176d753c8f1ff320757e3d
Alex Cerezo <alexcerezocontreras@gmail.com>
2026-06-03 09:09:29 +0200

    Reestructuración a React + integración cambios tiendas de compañero

--- BancoSol-Cliente/public/js/config.js
+++
@@ -1,6 +0,0 @@
-// Detecta automáticamente el dominio en el que se encuentra la aplicación
-if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-    window.API_URL = "http://localhost:3000";
-} else {
-    window.API_URL = window.location.origin;
-}
